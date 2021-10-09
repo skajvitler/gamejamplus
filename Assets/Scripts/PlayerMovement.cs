57069abbc7b773217abd3baf7b998085791ec4ea
AtanasovAleksandar <aleksandaratanasov43@gmail.com>
2021-10-10 01:59:18 +0200

    added player animation enemy animation and background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
 
 	public CharacterController2D controller;
+	private Rigidbody2D rb;
+	private Animator anim;
 
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
+	float value = 0f;
+
+	private void Start ()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+    }
 
 	// Update is called once per frame
 	void Update()
 	{
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		value = Input.GetAxis("Horizontal");
 
 		if (Input.GetButtonDown("Jump"))
 		{
@@ -32,6 +42,24 @@ public class PlayerMovement : MonoBehaviour
 		{
 			crouch = false;
 		}
+
+		if (value < 0 || value > 0)
+		{
+			anim.SetBool("isRunning", true);
+		}
+		else 
+		{
+			anim.SetBool("isRunning", false);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+			anim.SetTrigger("isJumping");
+        } 
+		else
+        {
+			anim.ResetTrigger("isJumping");
+        }
 
 	}
 
